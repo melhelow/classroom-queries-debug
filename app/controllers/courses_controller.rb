1d961d52cad4cee67e9af77820b1d0d605e1e8b9
Elhelw, Mohamed <114217025+melhelow@users.noreply.github.com>
2025-07-20 02:19:36 +0000

    routes,views,controllers,loops

--- app/controllers/courses_controller.rb
+++ app/controllers/courses_controller.rb
@@ -27,8 +27,8 @@ class CoursesController < ApplicationController
   end
 
   def update
-    id = params.fetch("path_id")
-    @course = Course.where({ :id => the_id }).at(0)
+      the_id = params.fetch("path_id") 
+      @course = Course.where({ :id => the_id }).at(0)
 
     @course.title = params.fetch("query_title")
     @course.term_offered = params.fetch("query_term_offered")
@@ -43,8 +43,8 @@ class CoursesController < ApplicationController
   end
 
   def destroy
-    the_id = params.fetch("path")
-    @course = Course.where({ :id => the_id }).at(0)
+    the_id = params.fetch("path_id")  
+  @course = Course.where({ :id => the_id }).at(0)
 
     @course.destroy
 
--- config/routes.rb
+++ config/routes.rb
@@ -4,7 +4,7 @@ Rails.application.routes.draw do
   #------------------------------
   # Department routes
 
-  get("/departments", { :controller => "department", :action => "index" })
+  get("/departments", { :controller => "departments", :action => "index" })
   
   get("/departments/:path_id", { :controller => "departments", :action => "show" })
 
@@ -23,9 +23,8 @@ Rails.application.routes.draw do
   
   get("/courses/:path_id", { :controller => "courses", :action => "show" })
 
-  get("/insert_course", { :controller => "courses", :action => "create" })
+  post("/insert_course", { :controller => "courses", :action => "create" })
 
-  get("/courses", { :controller => "courses_controller", :action => "index" })
   
   get("/modify_course/:path_id", { :controller => "courses", :action => "update" })
 
@@ -40,7 +39,6 @@ Rails.application.routes.draw do
 
   post("/insert_student", { :controller => "students", :action => "create" })
 
-  get("/students", { :controller => "students", :action => "index" })
   
   post("/modify_student/:path_id", { :controller => "students", :action => "update" })
 
